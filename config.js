e06a22c5c2c61ccecd0a2989efae5344ce856e79
Giancarlo Lapa <168876383+Giilack@users.noreply.github.com>
2026-08-09 22:55:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,4 @@
 // pero la confirmación no se guardará en una base de datos hasta completar esto.
 
 window.SUPABASE_URL = "https://litymruypwshkwjifrkm.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpdHltcnV5cHdzaGt3amlmcmttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyNzk1ODMsImV4cCI6MjEwMTg1NTU4M30.flHzzd3fbz4J0cpJM_E82qHLdcvD1ETQ6FE-VfSHZQI";
+window.SUPABASE_ANON_KEY = "sb_publishable_YAy9Gv8ca3z_qEAJaQx7hg_SLkvS1";
